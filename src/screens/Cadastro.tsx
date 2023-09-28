80559289f8b38d73c7f4e7ea9b4edde71cb42c55
Gustavo Fernandes <gugustavofernandes10@gmail.com>
2023-09-28 17:19:40 -0300

    adição do input de confirmação de senha

--- src/screens/Cadastro.tsx
+++ src/screens/Cadastro.tsx
@@ -5,9 +5,21 @@ import axios from 'axios';
 const CadastroScreen: React.FC = ({ navigation }: any) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [passwordsMatch, setPasswordsMatch] = useState(true);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const submit = async () => {
     try {
+      if (password !== confirmPassword) {
+        setErrorMessage('As senhas não coincidem');
+        setPasswordsMatch(false);
+        return;
+      }
+
+      setErrorMessage('');
+      setPasswordsMatch(true);
+
       const user = {
         email: email,
         password: password,
@@ -33,18 +45,26 @@ const CadastroScreen: React.FC = ({ navigation }: any) => {
         <View style={styles.cadastroBox}>
           <Text style={styles.title}>Cadastro</Text>
           <TextInput
-            placeholder="Email"
+            placeholder="Digite seu email"
             value={email}
             onChangeText={(text) => setEmail(text)}
             style={styles.input}
           />
           <TextInput
-            placeholder="Senha"
+            placeholder="Digite sua senha"
             value={password}
             onChangeText={(text) => setPassword(text)}
             secureTextEntry
-            style={styles.input}
+            style={[styles.input, !passwordsMatch && styles.inputError]}
           />
+          <TextInput
+            placeholder="Confirme sua senha"
+            value={confirmPassword}
+            onChangeText={(text) => setConfirmPassword(text)}
+            secureTextEntry
+            style={[styles.input, !passwordsMatch && styles.inputError]}
+          />
+          {!passwordsMatch && <Text style={styles.errorMessage}>{errorMessage}</Text>}
           <View style={styles.button}>
             <Button title="Cadastrar" onPress={submit} color="#A34704" />
           </View>
@@ -74,7 +94,7 @@ const styles = StyleSheet.create({
     color: 'blue'
   },
   cadastroBox: {
-    backgroundColor: 'rgba(255, 201, 74, 0.8)',
+    backgroundColor: 'rgba(255, 180, 74, 0.9)',
     padding: 20,
     borderRadius: 10,
     marginLeft: 20,
@@ -94,6 +114,15 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     paddingLeft: 10,
     backgroundColor: 'rgba(255, 255, 255, 0.7)',
+  },
+  inputError: {
+    borderColor: 'red',
+  },
+  errorMessage: {
+    color: 'red',
+    fontSize: 16,
+    marginTop: -10,
+    marginBottom: 6,
   },
   button: {
     marginBottom: 14,
